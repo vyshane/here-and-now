a832d8ff6abd2211b53045496d783141997fbd6f
Vy-Shane Xie <shane@node.mu>
2018-02-22 17:17:00 +0800

    Initial Commit

---
+++ Here & Now/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Here & Now
+//
+//  Created by Vy-Shane Xie on 22/2/18.
+//  Copyright © 2018 Vy-Shane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
