e42e0576dc7bae1b1b8983346fb60b465dabc911
Vy-Shane Xie <shane@node.mu>
2018-03-24 22:41:35 +0800

    Fix daylight detection via web service

--- Here and Now/UIScheme.swift
+++ Here and Now/UIScheme.swift
@@ -43,10 +43,10 @@ func uiScheme(fromLocation: Observable<CLLocation>, date: Observable<Date>) -> O
 
 func uiScheme(fromWeather: Observable<Weather>, date: Observable<Date>) -> Observable<UIScheme> {
     return Observable.combineLatest(fromWeather, date) { (w, d) in
-        if d > w.sunset && d < w.sunrise {
-            return .dark
+        if d > w.sunrise && d < w.sunset {
+            return .light
         }
-        return .light
+        return .dark
     }
 }
 
