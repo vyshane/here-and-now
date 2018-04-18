9366a6b9f3d412dfcdf910d16c9320ad25026deb
Vy-Shane Xie <shane@node.mu>
2018-04-18 22:38:09 +0800

    Components all the way down

--- Here and Now/Current Info/CurrentInfoViewController.swift
+++ Here and Now/Current Info/CurrentInfoViewController.swift
@@ -1,29 +1,45 @@
 //  Copyright © 2018 Vy-Shane Xie. All rights reserved.
 
+import CoreLocation
+import EasyPeasy
 import RxSwift
 
-class CurrentInfoViewController: UIViewController, CurrentInfoController {
-    private lazy var components: CurrentInfoComponents = initComponents(addToRootView: view, disposedBy: disposeBag)
+class CurrentInfoViewController: UIViewController {
     private var disposeBag = DisposeBag()
-    
+    private let locationManager = CLLocationManager()
+    private lazy var currentInfo = CurrentInfoComponent(disposedBy: disposeBag)
+    private let viewTransition: BehaviorSubject<Void> = BehaviorSubject(value: ())
+
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
+    override func viewDidLoad() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.distanceFilter = 20
+        locationManager.startUpdatingLocation()
+        view.addSubview(currentInfo.view)
+        currentInfo.view.easy.layout(Edges())
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
-        start(components: components, disposedBy: disposeBag)
+        currentInfo.start(
+            CurrentInfoComponent.Inputs(
+                authorization: locationManager.rx.didChangeAuthorization.asObservable(),
+                initialLocation: locationManager.rx.location.take(1),
+                date: currentDate().share(),
+                viewTransition: viewTransition
+            )
+        )
         super.viewWillAppear(animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        stop(components: components)
         disposeBag = DisposeBag()
         super.viewDidDisappear(animated)
     }
     
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        // Prevent flash of map background when reloading tiles after
-        // screen dimension or aspect ratio change
-        fadeIn(view: components.maskView, duration:0)
+        viewTransition.on(.next(()))
     }
 }
